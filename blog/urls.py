770f5456a424da14eb0e9b15ed50fa6bcf3bbcb4
Anastasiya Burykh <burykh_2000@mail.ru>
2023-09-22 21:40:22 +0300

    add blog

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+from blog.apps import BlogConfig
+from django.urls import path
+
+from blog.views import IndexView, BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
+
+app_name = BlogConfig.name
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('blogs/', BlogListView.as_view(), name='blog_list'),
+    path('blog_detail/<slug:slug>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/create/', BlogCreateView.as_view(), name='blog_create'),
+    path('blog/update/<int:pk>', BlogUpdateView.as_view(), name='blog_update'),
+    path('blog/delete/<int:pk>', BlogDeleteView.as_view(), name='blog_delete'),
+]
